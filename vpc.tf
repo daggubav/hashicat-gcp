96236d1e8d4fcd0cba1bc6a85ea613cb8333f734
daggubav <81985697+daggubav@users.noreply.github.com>
2021-04-05 16:15:31 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "~> 2.5.0"
+    version = "3.1.2"
     project_id   = var.project
     network_name = "gaurav-network"
 }
