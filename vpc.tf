2263205b94071b5b59cae6bbbd985f1484366532
daggubav <81985697+daggubav@users.noreply.github.com>
2021-04-05 15:34:20 -0400

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google//modules/vpc"
+    version = "~> 2.0.0"
+
+    project_id   = "var.project"
+    network_name = "gaurav-network"
+    shared_vpc_host = false
+    subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
