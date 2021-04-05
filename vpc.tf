7d4396155bd03d896933d8a72bbf2ae0a7fa563d
daggubav <81985697+daggubav@users.noreply.github.com>
2021-04-05 16:16:32 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,13 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "3.1.2"
+    version = "2.5.0"
     project_id   = var.project
     network_name = "gaurav-network"
+    subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
